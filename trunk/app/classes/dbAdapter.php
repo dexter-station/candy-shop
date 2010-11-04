3142f4cb9c0b81da654c4b9627e7af282e3e0c89
Dexter Station <user@domain>
2010-11-04 11:25:19 +0000

    Removing unused /svn folders

--- trunk/app/classes/dbAdapter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class dbAdapter extends PDO {
-
-    function  __construct($dsn = DB_DSN, $username = DB_USERNAME, $password = DB_PASSWORD) {
-        parent::__construct($dsn, $username, $password);
-    }
-
-}
